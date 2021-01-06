46057494d71c1baaac3145c34459d7f3e2796160
Saurav Kumar <ehsaurav@gmail.com>
2021-01-06 22:35:43 +0000

    -

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ password = input('Enter a password to check:\n')
 if re.fullmatch(r'[A-Za-z0-9@#$%^&+=]{8,}', password):
     print('Yay! you have a secure password!')
 else:
-    print('You password needs your attention!')
+    print('Your password needs your attention!')
